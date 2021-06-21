519376277d0ad153c2c5e6b7e1c0c344b4c64552
Rakib Raza <rakibraza159@gmail.com>
2021-06-21 10:50:51 +0600

    pagination done

--- src/components/Home/Jobs/Jobs.js
+++ src/components/Home/Jobs/Jobs.js
@@ -1,13 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, Grid, Typography } from "@material-ui/core";
+import { Box, Container, Grid, Typography, Button } from "@material-ui/core";
 import Job from "../Job/Job";
 const Jobs = () => {
   const [jobs, setJobs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [jobsPerPage] = useState(20);
+
+  const handlePrevBtn = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+  const handleNextBtn = () => {
+    const totalPages = [];
+    for (let i = 1; i <= Math.ceil(jobs.length / jobsPerPage); i++) {
+      totalPages.push(i);
+    }
+    if (totalPages.length > currentPage) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
   useEffect(() => {
     fetch("http://localhost:8000/approvedJobs")
       .then((res) => res.json())
       .then((data) => setJobs(data));
   }, []);
+
+  // Get current Jobs
+  const indexOfLastJobs = currentPage * jobsPerPage;
+  const indexOfFirstJobs = indexOfLastJobs - jobsPerPage;
+  const currentJobs = jobs.slice(indexOfFirstJobs, indexOfLastJobs);
   return (
     <Box pt={1} pb={5} style={{ background: "#f8f8f8" }}>
       <Container>
@@ -15,10 +37,14 @@ const Jobs = () => {
           Find Your Dream Jobs
         </Typography>
         <Grid container spacing={4}>
-          {jobs.map((job) => (
+          {currentJobs.map((job) => (
             <Job key={job._id} {...job} />
           ))}
         </Grid>
+        <Box mt={4} align='center'>
+          <Button onClick={handlePrevBtn}>prev</Button>
+          <Button onClick={handleNextBtn}>Next</Button>
+        </Box>
       </Container>
     </Box>
   );
